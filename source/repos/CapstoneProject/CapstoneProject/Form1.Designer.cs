1c8d333770f08d1bfa2e61bddc45aa891c3cb5fa
nholopoff <nicholasholopoff@gmail.com>
2018-01-25 02:32:25 -0600

    added file reading

---
+++ source/repos/CapstoneProject/CapstoneProject/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CapstoneProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scheduleDataGridView = new System.Windows.Forms.DataGridView();
+            this.ImportButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scheduleDataGridView
+            // 
+            this.scheduleDataGridView.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.scheduleDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.scheduleDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.scheduleDataGridView.Location = new System.Drawing.Point(39, 51);
+            this.scheduleDataGridView.Name = "scheduleDataGridView";
+            this.scheduleDataGridView.Size = new System.Drawing.Size(659, 150);
+            this.scheduleDataGridView.TabIndex = 0;
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Location = new System.Drawing.Point(338, 228);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(75, 23);
+            this.ImportButton.TabIndex = 1;
+            this.ImportButton.Text = "Import";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 263);
+            this.Controls.Add(this.ImportButton);
+            this.Controls.Add(this.scheduleDataGridView);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView scheduleDataGridView;
+        private System.Windows.Forms.Button ImportButton;
+    }
+}
+
---
+++ source/repos/CapstoneProject/CapstoneProject/Form1.cs
@@ -0,0 +1,93 @@
+﻿
+/* TODO
+ * DataSource to pull in information from spreadsheet
+ * Professor selection handled by listview
+ *  LAST NAME, FIRST NAME, ...
+ * Course output handled by either ListView or DataGrid depending on how much information is needed
+ * agree on a design template
+ * form goals of application
+ * when selected, output the classes they teach, otherwise display whitespace
+ * Make buttons work, EDIT ADD REMOVE
+ *  add failsafes in case user accidently removes wrong professor
+ * Esc throws "are you sure you want to exit? Yes or no"
+ * We can populate a DataGridView control with Excel spreadsheet data
+ *  OpenFileDialog needed to select spreadsheet data to view
+ * Add print capabilities
+ * Add compability with other file types
+ * CONSIDER PORTING TO EPPlus library instead of OLEDB
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapstoneProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Esc closes app */
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        /* Prompts user to select database file */
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdial = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer),
+                Title = "Course schedule",
+                Filter = "Microsoft Excel 2007-2013 XML (*.xlsx)|*.xlsx|All Files (*.*)|*.*", //update this as file types are supported
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            
+            // Fill DataTable with .xlsb file
+            if (fdial.ShowDialog() == DialogResult.OK)
+            {
+                if (fdial.FileName != null)
+                {
+                    try
+                    {
+                        string filename = fdial.FileName.ToString();
+                        string connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + filename + ";" + 
+                                                  "Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\""; //connectionstrings.com
+
+
+                        OleDbConnection connection = new OleDbConnection(connectionString); // establish link to file
+                        OleDbDataAdapter command = new OleDbDataAdapter("select * from [Sheet1$]", connection); // select every cell on Sheet1
+                        DataSet ds = new DataSet(); // initialize dataset for data grid
+                        command.Fill(ds); // fill data grid
+                        scheduleDataGridView.DataSource = ds.Tables[0];
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
+
